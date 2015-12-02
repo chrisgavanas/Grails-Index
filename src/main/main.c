4e2b5bfd0002eae5729235a64c0d41b2f36b7209
Chris <chrisgavanas@gmail.com>
2015-12-02 17:12:28 +0200

    rema,ed

--- src/main/main.c
+++ src/main/main.c
@@ -54,42 +54,5 @@ int main(int argc, char* argv[]) {
 	
 	closeFile(fptr);
 
-
-	return 0;
-
-
-/*	hashTable_t ht;
-	initHashTable(&ht);
-	
-	Node_t n1, n2, n3, n4, n5, n6, n7, n8, n9;
-
-	initNode(&n9, 9);	
-	initNode(&n8, 8);			
-	initNode(&n7, 7);		
-	initNode(&n6, 6);		
-	initNode(&n5, 5);		
-	initNode(&n4, 4);
-	initNode(&n3, 3);
-	initNode(&n2, 2);	
-	initNode(&n1, 1);
-	
-	insert(ht, n9);
-	insert(ht, n8);
-	insert(ht, n7);
-	insert(ht, n6);
-	insert(ht, n5);
-	insert(ht, n4);
-	insert(ht, n3);
-	insert(ht, n2);
-	insert(ht, n1);
-
-	sort(ht);
-	printHashTable(ht);
-
-	printf("%d\n", lookUp(ht, 5) == NULL);
-
-	deleteHashTable(&ht);
-*/
-
 	return 0;
 }
